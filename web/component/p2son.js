cd096a0dfb19c0917942e6ea55f249c05ac144ab
KevinBrother <1301239018@qq.com>
2019-10-25 16:48:13 +0800

    动画1

--- web/component/p2son.js
+++ web/component/p2son.js
@@ -4,7 +4,8 @@ var p2son = `
 <input v-model='name'/>
 <button v-on:click="detail.name">detail.name:   {{detail.name}}   msg: {{msg}}   count: {{ count }}</button>
 <input v-model='change.name'/>
-<input v-model='change.id'/>
+<input v-model='change.id'/> <br/>
+<button @click='click1'>儿子的按钮</button>
 </div>`
 
 Vue.component("p2son", {
@@ -12,11 +13,16 @@ Vue.component("p2son", {
     data: function() {
       return {
         count: 0,
-        change: this.detail
+        change: Object.assign({}, this.detail) 
       };
     },
     template: p2son,
     mounted() {
       this.change.name = '111'
+    },
+    methods: {
+      click1: function() {
+        alert('来自儿子的点击事件')
+      }
     }   
   });
